a7d412007fa7a31dbd501236dba17b673046db4b
Yana Byldina <yanabyldina@gmail.com>
2018-12-23 20:13:08 +0000

    Project Created

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
- document.querySelector('.get-json-ajax')
-      .addEventListener('click', getJsonAjax);
-      
-    function getJsonAjax() {
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const clientData = JSON.parse(xhr.responseText);
-                document.querySelector('.8008').innerText = clientData.name;
-             
-            }
-        }
-        xhr.open('GET', 'ral2.json', true);
-        xhr.send();
-    }
+ (function() {
+         document.querySelector('.page-loaded').innerText =
+             (new Date()).toLocaleTimeString();
+
+         document.querySelector('.get-json-ajax')
+             .addEventListener('click', getJsonAjax);
+
+         function getJsonAjax() {
+             const xhr = new XMLHttpRequest();
+             xhr.onreadystatechange = function() {
+                 if (xhr.readyState === 4 && xhr.status === 200) {
+                     const clientData = JSON.parse(xhr.responseText);
+                     document.querySelector('.client-name').innerText = clientData.name;
+                     document.querySelector('.balance').innerText = clientData.balance;
+                 }
+             }
+             xhr.open('GET', 'ral2.json', true);
+             xhr.send();
+         }
+ 
+ });
+ 
