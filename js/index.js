dfad7a46be0d6eb9d3f68b5dcb15a7057f8fdae3
Yana Byldina <yanabyldina@gmail.com>
2018-12-25 12:31:16 +0000

    Project Created

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
- (function() {
-         document.querySelector('.page-loaded').innerText =
-             (new Date()).toLocaleTimeString();
+$(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 700);
+        }
+    });
+});
 
-         document.querySelector('.get-json-ajax')
-             .addEventListener('click', getJsonAjax);
-
-         function getJsonAjax() {
-             const xhr = new XMLHttpRequest();
-             xhr.onreadystatechange = function() {
-                 if (xhr.readyState === 4 && xhr.status === 200) {
-                     const clientData = JSON.parse(xhr.responseText);
-                     document.querySelector('.client-name').innerText = clientData.name;
-                     document.querySelector('.balance').innerText = clientData.balance;
-                 }
-             }
-             xhr.open('GET', 'ral2.json', true);
-             xhr.send();
-         }
- 
- });
  
